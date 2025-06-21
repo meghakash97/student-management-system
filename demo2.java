d7a33812345bd5bf297336f904d65c9e2d057982
Soumya Sucharita Mohanty <meghakash.mohanty@gmail.com>
2025-06-21 22:26:02 +0530

    added one demo file

---
+++ demo2.java
@@ -0,0 +1,11 @@
+class demo2
+    {
+	void main()
+    
+       
+        {
+
+	System.out.println("My2nd demo program"); 
+       }
+}
+//for compile this program we have to write "java --enable-preview programname" in cmd prompt
